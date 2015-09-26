7504c43ef825c69e04760c7b079f1f8d107203c3
p-bms <p-bms@users.noreply.github.com>
2015-09-26 14:02:15 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/ImageurlStoreApplication.java
+++ src/main/java/org/bmshackathon/ImageurlStoreApplication.java
@@ -3,9 +3,17 @@ package org.bmshackathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ImageurlStoreApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ImageurlStoreApplication.class, args);
